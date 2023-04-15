40d161523188e20f6f39faf99f5513ca30eccbef
Thomaz Botelho <thomazrb@gmail.com>
2023-04-15 15:01:06 -0300

    Desafio da animação implicita funcionando!

--- lib/exercicios/animacao_implicita/animacao_implicita.dart
+++ lib/exercicios/animacao_implicita/animacao_implicita.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class AnimacaoImplicita extends StatefulWidget {
   const AnimacaoImplicita({super.key});
@@ -9,8 +8,38 @@ class AnimacaoImplicita extends StatefulWidget {
 }
 
 class _AnimacaoImplicitaState extends State<AnimacaoImplicita> {
+  bool toggle = true;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final width = toggle ? 140.0 : 70.0;
+    final radius = toggle ? 0.0 : 35.0;
+    final alignment = toggle ? Alignment.topCenter : Alignment.bottomRight;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Desafio do Botão Flutuante'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              toggle = !toggle;
+            });
+          },
+          child: AnimatedAlign(
+            duration: const Duration(seconds: 1),
+            alignment: alignment,
+            child: AnimatedContainer(
+              duration: const Duration(seconds: 1),
+              width: width,
+              height: 70,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(Radius.circular(radius))),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
